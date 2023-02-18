13ce29c13d347a62465c1c3a8c187c60feecf9fd
Gildeir L. Rodrigues <gildeirlopes@gmail.com>
2023-02-18 17:38:48 -0300

    Setting CSS and projet to PRD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,40 @@
-import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Todo } from './models/todo.model';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: String = 'Minhas Tarefas'
   public form: FormGroup;
+  public mode: String = 'list';
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      title: ["", Validators.compose ([
+      title: ['', Validators.compose([
         Validators.minLength(3),
         Validators.maxLength(60),
         Validators.required,
-      ])
-    ]})
+      ])]
+    });
 
     this.load();
-
   }
 
-  alteraTItle() {
-    this.title = 'alterei o cidadão';
+  changeMode(mode: String) {
+    this.mode = mode;
   }
 
   add() {
-    const title = this.form.controls["title"].value;
+    const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
     this.save();
-    this.clear()
+    this.clear();
+    this.changeMode('list');
   }
 
   clear() {
@@ -42,39 +42,34 @@ export class AppComponent {
   }
 
   remove(todo: Todo) {
-    const index = this.todos.indexOf(todo)
+    const index = this.todos.indexOf(todo);
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
     this.save();
   }
 
-  markAsDone(todo: Todo){
-    todo.done = true
+  markAsDone(todo: Todo) {
+    todo.done = true;
     this.save();
   }
 
-  markAsUndone(todo: Todo){
-    todo.done = false
-    console.log("undone", this.todos);
+  markAsUndone(todo: Todo) {
+    todo.done = false;
     this.save();
   }
 
   save() {
     const data = JSON.stringify(this.todos);
-    localStorage.setItem('todos', data)
+    localStorage.setItem('todos', data);
   }
 
-  load(){
-
-    const data = (localStorage.getItem('todos'))
-    
-    if (data)
-       this.todos = JSON.parse(data);
-    this.todos = [];
-
-
-
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
